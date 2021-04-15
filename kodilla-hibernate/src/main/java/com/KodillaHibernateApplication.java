81604980fe1d26594c72d97f97f2333f95b9deaa
Maciej Kupras <maciejkupras@wp.pl>
2021-04-15 11:17:27 +0200

    17.2_Hibernate

--- kodilla-hibernate/src/main/java/com/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/KodillaHibernateApplication.java
@@ -1,4 +1,4 @@
-package com.kodilla.kodillahibernate;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+    private static final String LISTNAME = "Hibernate";
+
+    @Test
+    void testTaskListDoeSave() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTaskList = taskListDao.findById(id);
+        assertTrue(readTaskList.isPresent());
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    void testTaskListDaoFindByListName() {
+        //Given
+
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListDao.save(taskList);
+        String name = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(name);
+
+        //Then
+        assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        taskListDao.deleteById(taskList.getId());
+
+
+
+
+    }
+}
